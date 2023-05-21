77018048371796ef53dc0719b2b3d4c27d2df9ea
Mateusz Loskot <mateusz.loskot@cadcorp.com>
2023-05-21 20:37:13 +0100

    Add Windows Server 2022 box based on gusztavvargadr/windows-server-2022-standard-core

---
+++ provision.ps1
@@ -0,0 +1,37 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidUsingPlainTextForPassword', '', Justification = 'Script used internally with dummy password')]
+[CmdletBinding(PositionalBinding = $False)]
+param
+(
+    [Parameter(Mandatory = $true)]
+    [string]$AdminUser,
+    [Parameter(Mandatory = $true)]
+    [string]$AdminPassword,
+    [Parameter(Mandatory = $true)]
+    [string]$NewHostname
+)
+
+Write-Host "[provision.ps1] Setting WinRM automatic start"
+Set-Service -Name WinRM -StartupType Automatic
+#sc config WinRM start= auto
+
+Write-Host "[provision.ps1] Disabling Windows Updates"
+Stop-Service wuauserv
+Set-Service wuauserv -StartupType Disabled
+
+Write-Host "[provision.ps1] Uninstalling Windows-Server-Antimalware"
+Uninstall-WindowsFeature -Name Windows-Server-Antimalware
+
+Write-Host "[provision.ps1] Uninstalling Windows Defender"
+Uninstall-WindowsFeature -Name Windows-Defender
+
+Write-Host "[provision.ps1] Uninstalling Windows Features"
+Uninstall-WindowsFeature System-DataArchiver # arbitrary pick to die-hard slim down of image
+Uninstall-WindowsFeature NET-WCF-Services45  # arbitrary pick to die-hard slim down of image
+
+Write-Host "[provision.ps1] Setting PowerShell as OpenSSH default shell"
+Set-ItemProperty -Path 'HKLM:\SOFTWARE\OpenSSH' -Name DefaultShell -Value "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe"
+
+Write-Host "[provision.ps1] Changing computer name to $NewHostname"
+$adminPasswordSecure = ConvertTo-SecureString $AdminPassword -AsPlainText -Force
+$adminCredential = New-Object System.Management.Automation.PSCredential ($AdminUser, $AdminPasswordSecure)
+Rename-Computer -NewName $NewHostname -DomainCredential $adminCredential
